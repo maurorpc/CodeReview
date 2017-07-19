4cc4bbf3441eb8bc39e840709ba348251ed59992
maurorpc <maurorpc@gmail.com>
2017-07-18 22:38:20 -0300

    Refactoring and Clasifying
    Classification of the Configuration and Database sections and same name refactoring.
    SQL file added, Database named 'CodeReview', Table named 'Logs'.

---
+++ CodeReview/Config/Application.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AppConfig = System.Configuration;
+
+namespace CodeReview.Config
+{
+    class Application
+    {
+        public Application()
+        {
+        }
+
+        public string ConnectionString
+        {
+            get
+            {
+                return Properties.Settings.Default.ConnectionString;
+            }
+        }
+
+        public string DirectoryPath
+        {
+            get
+            {
+                return Properties.Settings.Default.LogFileDirectory;
+            }
+        }
+    }
+}
---
+++ CodeReview/Database/RemoteServer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProgramSettings = System.Configuration.ConfigurationSettings;
+
+namespace CodeReview.Database
+{
+    class RemoteServer : LogsDataContext
+    {
+        private static string connectionString = new Config.Application().ConnectionString;
+
+        public RemoteServer() : base(connectionString)
+        {
+            //this.OnCreated();
+        }
+
+    }
+}
